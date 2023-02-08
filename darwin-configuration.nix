ab4005651b8368748a2b466cbb1c70702227976e
John Ott <john@johnott.us>
2023-02-08 12:12:03 -0800

    zsh: add bindkey for useful things

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -84,6 +84,16 @@ in
         export ATUIN_NOBIND="true"
         eval "$(atuin init zsh)"
         bindkey '^r' _atuin_search_widget
+
+        # Bind keyboard control characters to useful functions
+        # TODO: Understand why using zsh from nix breaks home / end / del
+        bindkey '^[[1~' beginning-of-line               # Home / Fn Left-Arrow
+        bindkey '^[[5~' beginning-of-buffer-or-history  # Page-up / Fn Up-Arrow
+        bindkey '^[[4~' end-of-line                     # End / Fn Right-Arrow
+        bindkey '^[[6~' end-of-buffer-or-history        # Page-down / Fn Down-Arrow
+        bindkey '^[[3~' delete-char                     # Del / Fn delete
+        bindkey '^[[1;3D' backward-word                 # Option Left-Arrow
+        bindkey '^[[1;3C' forward-word                  # Option Right-Arrow
       '';
 
       sessionVariables = {
