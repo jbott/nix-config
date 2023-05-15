13be48f89bca8204bdc4b643188911d96fd1e455
John Ott <john@johnott.us>
2023-05-15 01:51:13 -0700

    nix: add trusted-users = jbo

--- common/nix.nix
+++ common/nix.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = ["nix-command flakes"];
+    settings = {
+      experimental-features = ["nix-command flakes"];
+      trusted-users = ["jbo"];
+    };
     distributedBuilds = true;
     extraOptions = ''
       builders-use-substitutes = true
