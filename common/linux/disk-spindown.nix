dab8ad108a00141148056df860af2e10bc48c846
John Ott <john@johnott.us>
2023-05-17 05:05:18 +0000

    nix fmt

--- common/linux/disk-spindown.nix
+++ common/linux/disk-spindown.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   powerManagement.powerUpCommands = ''
     for f in $(${pkgs.util-linux}/bin/lsblk --nodeps --noheadings --output NAME --paths | ${pkgs.gnugrep}/bin/grep sd); do
       ${pkgs.hdparm}/sbin/hdparm -S 120 "''${f}"
