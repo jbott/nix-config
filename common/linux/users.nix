4bd1150cec59ef3c3dfb2c4a97825dabd1ac1ad4
John Ott <john@johnott.us>
2023-05-25 19:00:48 +0000

    user: add jbo to dialout

--- common/linux/users.nix
+++ common/linux/users.nix
@@ -8,7 +8,10 @@
       name = "jbo";
       home = "/home/jbo";
       group = "jbo";
-      extraGroups = ["wheel"];
+      extraGroups = [
+        "dialout"
+        "wheel"
+      ];
       shell = pkgs.zsh;
       initialHashedPassword = "\$6\$QUiXYT6XzD39mklJ\$ZaA07XGXhcbaE7KB6WZBtKDpN0ZYRz/WayP038wxSDyU6E15zEOBpVCK3ChHCxvmokyeIDcc/3XkLBAZpHd5D0";
       openssh.authorizedKeys.keys = [
