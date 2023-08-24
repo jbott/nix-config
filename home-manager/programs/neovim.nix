3d42df7e8f00f8b9f0852f0a8e465176beae6dfe
John Ott <john@johnott.us>
2023-08-24 09:16:50 -0700

    add vim-auto-mkdir

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -58,6 +58,20 @@ in {
 
       " Do not auto-open markdown composer
       let g:markdown_composer_autostart = 0
+
+      " vim-auto-mkdir plugin from
+      " https://github.com/travisjeffery/vim-auto-mkdir/
+      augroup vim-auto-mkdir
+        autocmd!
+        autocmd BufWritePre * call s:auto_mkdir(expand('<afile>:p:h'), v:cmdbang)
+        function! s:auto_mkdir(dir, force)
+          if !isdirectory(a:dir)
+                \   && (a:force
+                \       || input("'" . a:dir . "' does not exist. Create? [y/N]") =~? '^y\%[es]$')
+            call mkdir(iconv(a:dir, &encoding, &termencoding), 'p')
+          endif
+        endfunction
+      augroup END
     '';
     plugins = with pkgs.vimPlugins; let
     in [
