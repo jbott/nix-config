c4074ffe474c0c17a82b923e8ef804bf016a4dfc
John Ott <john@johnott.us>
2023-12-17 00:12:55 -0800

    lapsed-pacifist: add hacky systemd user mode support for non-declarative testing

--- machines/Lapsed-Pacifist/default.nix
+++ machines/Lapsed-Pacifist/default.nix
@@ -16,4 +16,15 @@
 
   # Do not compress with zstd since I normally build this locally
   sdImage.compressImage = false;
+
+  # ===== Hacky config for some HAP thing I don't want to fully codify yet ===== #
+  # HAP advertising port
+  networking.firewall.allowedTCPPorts = [ 51926 ];
+
+  # Avahi to enable HAP discovery
+  services.avahi.enable = true;
+  services.avahi.publish.enable = true;
+
+  # Enable linger so systemd user units start at boot
+  users.users.jbo.linger = true;
 }
