049933c61074a655db5cde9cd4088de35d1d8053
John Ott <john@johnott.us>
2024-01-09 17:58:25 -0800

    service/home-assistant: pullImage so HA can start without internet, update to 2024.1.2

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -1,8 +1,19 @@
-{
+{pkgs, ...}: let
+  imageName = "ghcr.io/home-assistant/home-assistant";
+  imageTag = "2024.1.2";
+  imageDigest = "sha256:59c89e5b6c0db63b8a7705f82fcb12dfda41b1c94a63ad9430b70f55106706d8";
+  sha256 = "sha256-MrreRklXYrBONIAfN8ShFcMiELL1aBpYuylOJ+MlUa4=";
+
+  image = "${imageName}:${imageTag}";
+  imageFile = pkgs.dockerTools.pullImage {
+    inherit imageName imageDigest sha256;
+    finalImageTag = imageTag;
+  };
+in {
   virtualisation.oci-containers = {
     backend = "podman";
     containers.home-assistant = {
-      image = "ghcr.io/home-assistant/home-assistant:2023.5.3";
+      inherit image imageFile;
       environment = {
         TZ = "America/Los_Angeles";
       };
