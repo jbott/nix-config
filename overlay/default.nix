fbde0c4cd32b71698420ccb94c1dd91616dff029
John Ott <john@johnott.us>
2024-01-19 17:03:43 -0800

    overlay: revert pip to 23.2.1
    This broke pip-tools for unknown reasons that I don't want to fix
    correctly.

--- overlay/default.nix
+++ overlay/default.nix
@@ -3,4 +3,7 @@
   deconz-aarch64 = self.qt5.callPackage ./pkgs/deconz-aarch64 {};
   deploy-nixos = self.callPackage ./pkgs/deploy-nixos {};
   m1ddc = self.callPackage ./pkgs/m1ddc {};
+
+  python311 = super.python311.override {packageOverrides = import ./pkgs/python-modules;};
+  python312 = super.python312.override {packageOverrides = import ./pkgs/python-modules;};
 })
