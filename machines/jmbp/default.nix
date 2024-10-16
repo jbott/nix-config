5fbd2f295a9cfe447320de53dd1c90eb9909207d
John Ott <john@johnott.us>
2024-10-16 12:28:46 -0700

    machines/jmbp: configure pam_reattach in sudo_local

--- machines/jmbp/default.nix
+++ machines/jmbp/default.nix
@@ -1,8 +1,19 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   system.stateVersion = 5;
 
-  # Enable pam_tid.so
-  security.pam.enableSudoTouchIdAuth = true;
+  # Enable pam_reattach and pam_tid
+  # Inspired by the changes in https://github.com/LnL7/nix-darwin/pull/1020
+  environment.etc."pam.d/sudo_local" = {
+    enable = true;
+    text = ''
+      auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+      auth       sufficient     pam_tid.so
+    '';
+  };
 
   home-manager.users.jbo.programs.zsh.sessionVariables = {
     SSH_AUTH_SOCK = "/Users/jbo/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
