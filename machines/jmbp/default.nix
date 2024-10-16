4cc0021be08f594238e14a581ea656e97dd28512
John Ott <john@johnott.us>
2024-10-16 11:54:58 -0700

    machines/jmbp: set SSH_AUTH_SOCK by default

--- machines/jmbp/default.nix
+++ machines/jmbp/default.nix
@@ -3,4 +3,8 @@
 
   # Enable pam_tid.so
   security.pam.enableSudoTouchIdAuth = true;
+
+  home-manager.users.jbo.programs.zsh.sessionVariables = {
+    SSH_AUTH_SOCK = "/Users/jbo/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+  };
 }
