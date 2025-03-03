16394d78a765c03ed734d486222cfb9918ff25d8
John Ott <john@johnott.us>
2025-03-03 12:17:23 -0800

    darwin/nix: remove unnecessary config

--- common/darwin/nix-darwin.nix
+++ common/darwin/nix-darwin.nix
@@ -1,7 +1,4 @@
 {...}: {
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-
   # This is the best we can do, the darwin config is really fake at this point
   # NOTE: this might actually be _wrong_ but I'll see later lol
   environment.darwinConfig = "$HOME/src/nix-config";
