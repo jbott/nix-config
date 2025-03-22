160f04045f4be24dba0b8fa98a85f766c42e61ca
John Ott <john@johnott.us>
2025-03-22 13:24:35 -0700

    atuin: enable daemon mode

--- home-manager/programs/atuin.nix
+++ home-manager/programs/atuin.nix
@@ -1,11 +1,18 @@
 {
   programs.atuin = {
     enable = true;
+
+    # Use normal scrollback on up arrow
     flags = [
       "--disable-up-arrow"
     ];
+
+    # Disable the update check since we're managing the version with nix
     settings = {
       update_check = false;
     };
+
+    # Enable the atuin daemon to speed up database access on zfs
+    daemon.enable = true;
   };
 }
