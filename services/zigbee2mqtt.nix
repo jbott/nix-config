cbfa7c54bca903b9cab1f05667acca71ffe3cb89
John Ott <john@johnott.us>
2025-03-23 22:15:30 -0700

    services/zigbee2mqtt: init

---
+++ services/zigbee2mqtt.nix
@@ -0,0 +1,23 @@
+{
+  # Enable mosquitto mqtt server
+  services.mosquitto.enable = true;
+
+  # Enable zigbee2mqtt
+  services.zigbee2mqtt = {
+    enable = true;
+    settings = {
+      homeassistant.enable = true;
+
+      serial = {
+        port = "/dev/serial/by-id/usb-dresden_elektronik_ingenieurtechnik_GmbH_ConBee_II_DE2250332-if00";
+        adapter = "deconz";
+      };
+    };
+  };
+
+  # Use a bind mount to map data onto /persist
+  fileSystems."/var/lib/zigbee2mqtt" = {
+    device = "/persist/var/lib/zigbee2mqtt";
+    options = ["bind"];
+  };
+}
