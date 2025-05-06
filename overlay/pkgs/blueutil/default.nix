445b305425270bc10955e632cd6a216085d0de9f
John Ott <john@johnott.us>
2025-05-06 14:36:12 -0700

    overlay/blueutil: remove now unnecessary apple_sdk

--- overlay/pkgs/blueutil/default.nix
+++ overlay/pkgs/blueutil/default.nix
@@ -17,8 +17,6 @@ in
       hash = "sha256-dxsgMwgBImMxMMD+atgGakX3J9YMO2g3Yjl5zOJ8PW0=";
     };
 
-    buildInputs = with darwin.apple_sdk.frameworks; [IOBluetooth];
-
     # The makefile incorrectly (for GNUMake, at least) specifies CFLAGS rather
     # than OBJCFLAGS, so inline a copy here
     OBJCFLAGS = "-mmacosx-version-min=10.9 -framework Foundation -framework IOBluetooth";
