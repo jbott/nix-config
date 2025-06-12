e86c6b1d1f259f83f9f612b67b8e4b035253e4a9
John Ott <john@johnott.us>
2025-06-12 10:50:39 -0700

    treefmt/typos: enable and add config file

--- tools/treefmt.nix
+++ tools/treefmt.nix
@@ -1,13 +1,21 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  typosConfigPath = ../.typos.toml;
+  typosConfigContent = builtins.fromTOML (builtins.readFile typosConfigPath);
+in {
   projectRootFile = "flake.nix";
 
   programs = {
     alejandra.enable = true;
     just.enable = true;
     shellcheck.enable = true;
+    typos.enable = true;
   };
 
   settings = {
+    formatter.typos = {
+      configFile = typosConfigPath;
+      excludes = typosConfigContent.files.extend-exclude;
+    };
     formatter.shellcheck.includes = [
       "home-manager/scripts/bin/*"
       "tools/bin/*"
