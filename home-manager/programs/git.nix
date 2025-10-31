1e155b82d523cd49d576dac64636cbe8c1ff2707
John Ott <john@johnott.us>
2025-10-31 13:16:15 -0700

    home-manager/git: fix deprecated settings

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -3,32 +3,34 @@
     enable = true;
     lfs.enable = true;
 
-    userName = "John Ott";
-    userEmail = "john@johnott.us";
+    settings = {
+      user = {
+        name = "John Ott";
+        email = "john@johnott.us";
+      };
 
-    aliases = {
-      c = "commit";
-      cfp = "commit-for-project";
-      cm = "commit -m";
-      co = "checkout";
-      cp = "cherry-pick";
-      di = "diff";
-      ds = "diff --staged";
-      fc = "fzf-checkout";
-      fo = "fzf-oneline";
-      fwl = "push --force-with-lease";
-      o = "log --oneline";
-      poh = "push -u origin HEAD";
-      pohfwl = "push -u origin HEAD --force-with-lease";
-      ri = "rebase -i";
-      rim = "rebase -i origin/main";
-      rom = "rebase origin/main";
-      rup = "remote update -p";
-      rur = "!git remote update && git rebase -i origin/main";
-      st = "status";
-    };
+      alias = {
+        c = "commit";
+        cfp = "commit-for-project";
+        cm = "commit -m";
+        co = "checkout";
+        cp = "cherry-pick";
+        di = "diff";
+        ds = "diff --staged";
+        fc = "fzf-checkout";
+        fo = "fzf-oneline";
+        fwl = "push --force-with-lease";
+        o = "log --oneline";
+        poh = "push -u origin HEAD";
+        pohfwl = "push -u origin HEAD --force-with-lease";
+        ri = "rebase -i";
+        rim = "rebase -i origin/main";
+        rom = "rebase origin/main";
+        rup = "remote update -p";
+        rur = "!git remote update && git rebase -i origin/main";
+        st = "status";
+      };
 
-    extraConfig = {
       init.defaultBranch = "main";
     };
 
