f3fb7ca941dcd17cc89df4d03f2f97be112204b5
John Ott <john@johnott.us>
2026-02-06 12:41:04 -0800

    home-manager/jujutsu: add restack and lt aliases

--- home-manager/programs/jujutsu.nix
+++ home-manager/programs/jujutsu.nix
@@ -10,9 +10,11 @@
 
       aliases = {
         dt = ["diff" "--from" "latest(heads(::@ & ::trunk()))" "--to" "@"];
+        lt = ["log" "-r" "roots(trunk()..@)-::@"];
+        push = ["git" "push" "-r" "::@ & bookmarks()"];
+        restack = ["rebase" "-o" "trunk()" "-s" "roots(trunk()..) & mutable()"];
         rom = ["rebase" "-d" "main" "--skip-emptied"];
         tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
-        push = ["git" "push" "-r" "::@ & bookmarks()"];
       };
 
       ui = {
