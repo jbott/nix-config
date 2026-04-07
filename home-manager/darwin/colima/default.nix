b0784f97ef47e064955495004c4ec438b3afb446
John Ott <john@johnott.us>
2026-04-07 16:48:59 -0700

    home-manager/colima: replace vendored yaml with yq merge of upstream defaults

--- home-manager/darwin/colima/default.nix
+++ home-manager/darwin/colima/default.nix
@@ -1,3 +1,20 @@
-{
-  home.file.".colima/_templates/default.yaml".source = ./default.yaml;
+{pkgs, ...}: let
+  yaml = pkgs.formats.yaml {};
+  settings = {
+    cpu = 8;
+    memory = 16;
+    kubernetes.enabled = true;
+    docker.features.containerd-snapshotter = true;
+    vmType = "vz";
+    rosetta = true;
+    mountType = "virtiofs";
+  };
+in {
+  home.file.".colima/_templates/default.yaml".source = pkgs.runCommand "colima-template" {
+    nativeBuildInputs = [pkgs.yq-go];
+  } ''
+    yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' \
+      ${pkgs.colima.src}/embedded/defaults/colima.yaml \
+      ${yaml.generate "colima-overrides.yaml" settings} > $out
+  '';
 }
