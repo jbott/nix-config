c481907893fe438276900d520636a007faaa5bf4
John Ott <john@johnott.us>
2026-05-20 05:58:40 +0000

    services/homebridge: bump image to 2026-05-13

--- services/homebridge.nix
+++ services/homebridge.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: let
   imageName = "docker.io/homebridge/homebridge";
-  imageTag = "2025-02-26";
-  imageDigest = "sha256:97d5c72d3eaf5f5241934feecd07e15704af218819696c1bd867b7b3a6953b3c";
-  sha256 = "sha256-UP2oFOOS9i5oePwNUr0xt8TM7EZBajdXczPbu+BfoKs=";
+  imageTag = "2026-05-13";
+  imageDigest = "sha256:36d43ed9711e37fba7a2ab82165c8f3b199398fd6d491c9eeaf289d43850e927";
+  sha256 = "sha256-boHm3odcXklObiJcQMFtQm+K0IDeyUh2crPIew47El0=";
 
   image = "${imageName}:${imageTag}";
   imageFile = pkgs.dockerTools.pullImage {
